bc29c73af559a650a123e1bd9af661efddd4fe88
Kenny Tran <kennyytran95@gmail.com>
2022-02-06 04:52:19 -0800

    Add dummy data

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const dummyData = [
+  {
+    title: 'Fake Title',
+    body: 'Insert a lot of really cool text here.',
+    date: '2/6/2022',
+  },
+  {
+    title: '2',
+    body: 'Insert a lot of really cool text here.',
+    date: '2/6/2022',
+  },
+  {
+    title: '3',
+    body: 'Insert a lot of really cool text here.',
+    date: '2/6/2022',
+  },
+  {
+    title: '4',
+    body: 'Insert a lot of really cool text here.',
+    date: '2/6/2022',
+  },
+  {
+    title: '5',
+    body: 'Insert a lot of really cool text here.',
+    date: '2/6/2022',
+  }
+];
+/* render information into DOM (each into their own box)
+style it
+*/
